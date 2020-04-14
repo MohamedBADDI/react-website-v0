7e101b722fed83acfa871a2500626dd964abb259
MohamedBADDI <baddisimo@gmail.com>
2020-04-14 13:46:51 +0100

    Updates

---
+++ precache-manifest.577b1886e316d6551dd3b393797cfe9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806b5f09d705dc81845edb4ff2be2bac",
+    "url": "/react-website-v0/index.html"
+  },
+  {
+    "revision": "a87a700e0df98f12d2a8",
+    "url": "/react-website-v0/static/css/main.6619bc70.chunk.css"
+  },
+  {
+    "revision": "14fc8713b79c9d659dbd",
+    "url": "/react-website-v0/static/js/2.9885a20f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website-v0/static/js/2.9885a20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87a700e0df98f12d2a8",
+    "url": "/react-website-v0/static/js/main.5d2a15ef.chunk.js"
+  },
+  {
+    "revision": "72f8485bd451ecea316c",
+    "url": "/react-website-v0/static/js/runtime-main.22e974cd.js"
+  }
+]);
